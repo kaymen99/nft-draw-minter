85902386f74e8fcf4f52d18e07ccb41a4c432c3a
Aymen <AymenMir1001@gmail.com>
2022-05-28 02:22:59 +0100

    Update contracts-config.js

--- src/utils/contracts-config.js
+++ src/utils/contracts-config.js
@@ -1,4 +1,5 @@
 
   export const contractAddress = "0x5EebceC88C2380f38ed06Bea63AD3aEEd8626f4E"
   export const ownerAddress = "0x877a4834285eC37ea4094F8689bE0e0325683c25"
+  export const networkDeployedTo = "1337"
   
