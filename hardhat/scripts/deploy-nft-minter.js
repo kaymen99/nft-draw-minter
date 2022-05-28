910ff4ebefa7734dabee3660d4f806c7befb1c87
Aymen <AymenMir1001@gmail.com>
2022-05-28 02:19:51 +0100

    Update deploy-nft-minter.js

--- hardhat/scripts/deploy-nft-minter.js
+++ hardhat/scripts/deploy-nft-minter.js
@@ -13,11 +13,12 @@ async function main() {
   await nftContract.deployed();
   console.log("nft contract deployed to:", nftContract.address);
 
-  /* this code writes the contract addresses to a local */
+  /* this code writes the contract addresses and network deployed in to a local */
   /* file named config.js that we can use in the app */
   fs.writeFileSync('../src/utils/contracts-config.js', `
   export const contractAddress = "${nftContract.address}"
   export const ownerAddress = "${nftContract.signer.address}"
+  export const networkDeployedTo = "${hre.network.config.chainId}"
   `)
 }
 
