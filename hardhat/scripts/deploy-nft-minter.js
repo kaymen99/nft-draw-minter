0eee7ab1c2c87df203164c2df366378b31a07a95
Aymen <AymenMir1001@gmail.com>
2022-06-08 23:34:07 +0100

    Update deploy-nft-minter.js

--- hardhat/scripts/deploy-nft-minter.js
+++ hardhat/scripts/deploy-nft-minter.js
@@ -1,6 +1,9 @@
 /* scripts/deploy.js */
 const hre = require("hardhat");
 const fs = require('fs');
+const { verify } = require('../utils/verify')
+
+const LOCAL_NETWORKS = ["localhost", "ganache"]
 
 async function main() {
   let mintingFee = hre.ethers.utils.parseEther("0.001", "ether");
@@ -20,6 +23,11 @@ async function main() {
   export const ownerAddress = "${nftContract.signer.address}"
   export const networkDeployedTo = "${hre.network.config.chainId}"
   `)
+  
+  if (!LOCAL_NETWORKS.includes(hre.network.name) && hre.config.etherscan.apiKey !== "") {
+    await nftContract.deployTransaction.wait(6)
+    await verify(nftContract.address, [mintingFee, nftName, nftSymbol])
+  }
 }
 
 main()
